56ac10410522ca0f07f3798081804706d44b461d
Subash <subash@subash>
2016-07-12 19:35:31 +0530

    status bar added, input output customzed

--- imagemove.php
+++ imagemove.php
@@ -3,14 +3,29 @@ $dir = "myim";
 $movDir = "myphoto";
 echo "\n Read all the Image from DIR\n";
 echo "*********************************\n\n";
+
+$dir = readline("Input Folder Name : ");
+$movDir = readline("Output Folder Name : ");
+
+
 $files1 = scandir($dir);
 array_shift($files1);
 array_shift($files1);
-if (count($files1) > 0) {
+$totalImages = count($files1);
+$completedItems = 0;
+echo "Total Items Found ". $totalImages;
+echo "\n\n";
+if ($totalImages > 0) {
     foreach ($files1 as $file) {
-        echo "\n$file\n";
         
-        $fileDetails = exif_read_data($dir . "/" . $file);
+        
+        $fileDetails = @exif_read_data($dir . "/" . $file);
+        if(!$fileDetails) {
+            echo "Not able to read image property\n\n";
+            $completedItems++;
+            show_status($completedItems,$totalImages);
+            continue;
+        }
         // print_r($fileDetails);exit;
         $dt = new DateTime();
         $dt->setTimestamp($fileDetails['FileDateTime']);
@@ -29,9 +44,58 @@ if (count($files1) > 0) {
             mkdir("$movDir/" . $Yeardir . "/$monthdir/$daydir", 0777);
         }
         $photoDir = $Yeardir . "/$monthdir/$daydir";
-        If (copy("$dir/" . $fileDetails['FileName'], "$movDir/" . $photoDir . "/" . $dt->format('d_j_H_i_s') . "_" . $fileDetails['FileName'])) {
+        If (copy("$dir/" . $fileDetails['FileName'], "$movDir/" . $photoDir . "/" . $dt->format('d_F_H_i_s') . "_" . $fileDetails['FileName'])) {
            // unlink($dir . "/" . $file);
         }
-        print "\n\n" . $dt->format('j_M_Y_H_i_s');
+        //print "\n\n" . $dt->format('j_M_Y_H_i_s');
+        $completedItems++;
+        show_status($completedItems,$totalImages);
     }
+}
+
+
+function show_status($done, $total, $size=30) {
+ 
+    static $start_time;
+ 
+    // if we go over our bound, just ignore it
+    if($done > $total) return;
+ 
+    if(empty($start_time)) $start_time=time();
+    $now = time();
+ 
+    $perc=(double)($done/$total);
+ 
+    $bar=floor($perc*$size);
+ 
+    $status_bar="\r[";
+    $status_bar.=str_repeat("=", $bar);
+    if($bar<$size){
+        $status_bar.=">";
+        $status_bar.=str_repeat(" ", $size-$bar);
+    } else {
+        $status_bar.="=";
+    }
+ 
+    $disp=number_format($perc*100, 0);
+ 
+    $status_bar.="] $disp%  $done/$total";
+ 
+    $rate = ($now-$start_time)/$done;
+    $left = $total - $done;
+    $eta = round($rate * $left, 2);
+ 
+    $elapsed = $now - $start_time;
+ 
+    $status_bar.= " remaining: ".number_format($eta)." sec.  elapsed: ".number_format($elapsed)." sec.";
+ 
+    echo "$status_bar  ";
+ 
+    flush();
+ 
+    // when done, send a newline
+    if($done == $total) {
+        echo "\n";
+    }
+ 
 }
